d75977e926cd25748c6dc5889bff3127413451a1
John Kyle Joseph Reyes <jkj313.kr@gmail.com>
2023-10-03 20:41:47 +0800

    part 2 update

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,31 +1,10 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import { HousingLocation } from '../housinglocation';
+import { Injectable, inject } from '@angular/core';
+import { HousingLocation } from './housinglocation';
 
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
-        [housingLocation]="housingLocation"
-      >
-      </app-housing-location>
-    </section>
-  `,
-
-  styleUrls: ['./home.component.css'],
+@Injectable({
+  providedIn: 'root'
 })
-export class HomeComponent {
+export class HousingService {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
   housingLocationList: HousingLocation[] = [
     {
@@ -129,4 +108,15 @@ export class HomeComponent {
       laundry: true,
     },
   ];
+
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+    }
+    getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+    }
+    housingService: HousingService = inject(HousingService);
+      
+
+  constructor() { }
 }
